de1d00fa7ef4bec17f15639c8abddc086d76eadc
CIFuzz <cifuzz@clusterfuzz.com>
2024-08-23 01:44:16 +0000

    Corpus upload

--- corpus/luaL_loadbuffer_proto_test/bench_14.lua
+++
@@ -1,9 +0,0 @@
--- Public Domain, 2020
--- Source: https://gitspartv.github.io/LuaJIT-Benchmarks/
-
-local T = {}
-local CachedTable = {"abc", "def", "ghk"}
-
-T[times] = CachedTable
-
-T[times] = {"abc", "def", "ghk"}
--- corpus/luaL_loadbuffer_proto_test/bench_2.lua
+++
@@ -1,6 +0,0 @@
--- Public Domain, 2020
--- Source: https://gitspartv.github.io/LuaJIT-Benchmarks/
-
-local s = math.sin
-math.sin(3.14)
-s(3.14)
